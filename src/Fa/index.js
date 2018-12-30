b0d8b573cf4ad461bedc140235d58d6c588e9ca5
Dimitri Kopriwa <dka@dev-12.yeutech.vn>
2018-12-31 03:11:05 +0700

    fix(font-awesome): added missing size to PropTypes

--- src/Fa/index.js
+++ src/Fa/index.js
@@ -1,38 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
-import styled from 'styled-components';
 
-export const propTypes = {
-  /**
-   * @ignore
-   */
-  className: PropTypes.string,
-  /** Size variables. Can be: */
-  size: PropTypes.oneOf([
-    'lg',
-    '2x',
-    '3x',
-    '4x',
-    '5x',
-  ]),
-  /** Color variables. Can be: */
-  color: PropTypes.oneOf([
-    'success',
-    'info',
-    'warning',
-    'danger',
-    'white',
-    'gray-dark',
-    'primary',
-    'hide',
-  ]),
-};
+/**
+ * Font awesome component
+ */
+export default class Fa extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  static propTypes = {
+    /** @ignore */
+    className: PropTypes.string,
+    /** Size variables. Can be: */
+    size: PropTypes.oneOf([
+      'xs',
+      'sm',
+      'lg',
+      '2x',
+      '3x',
+      '4x',
+      '5x',
+      '7x',
+      '10x',
+    ]),
+    /** Color variables. Can be: */
+    color: PropTypes.oneOf([
+      'success',
+      'info',
+      'warning',
+      'danger',
+      'white',
+      'gray-dark',
+      'primary',
+      'hide',
+    ]),
+  };
 
-class FaUnstyled extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  /* eslint-disable react/no-unused-prop-types */
-  static propTypes = propTypes;
-  /* eslint-enable react/no-unused-prop-types */
+  static defaultProps = {
+    size: null,
+    color: null,
+  };
 
   render() {
     const {
@@ -54,14 +59,3 @@ class FaUnstyled extends React.Component { // eslint-disable-line react/prefer-s
     );
   }
 }
-
-const Fa = styled(FaUnstyled)`
-  &.fa-lg {
-    vertical-align: -2%;
-  }
-`;
-
-Fa.propTypes = propTypes;
-
-/** @component */
-export default Fa;
